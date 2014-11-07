aaf591a6968cdac5d8488a4b68f4a48b8b74a249
Andreas Hollandt <trass3r@gmail.com>
2014-11-07 11:15:11 +0100

    init

---
+++ Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace CppTestRunner
+{
+    static class GuidList
+    {
+		public const string guidCppTestRunnerPkgString = "8b49192b-e6cc-4e33-80dc-f44fd4c121c0";
+		public const string guidCppTestRunnerCmdSetString = "2d47905a-7df7-4ce8-9203-c8029ed4b5c1";
+
+		public static readonly Guid guidCppTestRunnerCmdSet = new Guid(guidCppTestRunnerCmdSetString);
+    };
+}
